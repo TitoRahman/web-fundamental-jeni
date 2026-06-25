25ab0fa4389924aabef05a4f62b80122808862ed
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2026-06-25 20:32:04 +0700

    Added note for 10th Meeting

---
+++ M10 - JS Basic/script.js
@@ -0,0 +1,25 @@
+// INPUT
+// VARIABLE - penyimpan nilai, dengan tipe data tertentu
+// INT - Angka bulat
+// Float - Angka koma
+// Boolean - True/False
+// String - Text (Tanda quote)
+
+// let name =  prompt("What is your name?");
+// // PROSES
+// // OUTPUT
+// console.log(name)
+// Konversi Tipe Data
+
+let A =  Number(prompt("Inserty \"A\" : "));
+let B =  Number(prompt("Insert B : "));
+// PROSES
+let Total = A + B;
+// * / + - %(Modulus)
+
+// !== / !=
+// === / ==
+
+
+// OUTPUT
+console.log(`Total dari A + B adalah \${} ${Total}`)
