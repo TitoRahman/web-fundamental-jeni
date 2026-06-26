39e1ef14860c9f4242d4e0ca1977b791fae75cd0
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2026-06-26 20:30:34 +0700

    Added note for 11th Meeting

--- M10 - JS Basic/script.js
+++ M10 - JS Basic/script.js
@@ -11,15 +11,27 @@
 // console.log(name)
 // Konversi Tipe Data
 
-let A =  Number(prompt("Inserty \"A\" : "));
-let B =  Number(prompt("Insert B : "));
-// PROSES
-let Total = A + B;
-// * / + - %(Modulus)
+// let A =  Number(prompt("Inserty \"A\" : "));
+// let B =  Number(prompt("Insert B : "));
+// // PROSES
+// let Total = A + B;
+// // * / + - %(Modulus)
+//
+// // !== / !=
+// // === / ==
+//
+//
+// // OUTPUT
+// console.log(`Total dari A + B adalah \${} ${Total}`)
 
-// !== / !=
-// === / ==
 
+// True / False
 
-// OUTPUT
-console.log(`Total dari A + B adalah \${} ${Total}`)
+// > < <= >= == !=
+// && and . || or
+let a = 100; // true = 1 | false = 0
+let b = 100; // true = 1 | false = 0
+
+const res = false || true ;
+const PI = 3.14;
+console.log(res);
---
+++ M11 - JS Flow Control I/script.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 15;
+
+if (a > b) {
+    console.log("A is geateer")
+} else if (a < b) {
+    console.log("B is bigger")
+} else {
+    console.log("The same")
+}
+
+if (a%2 == 0) {
+    console.log("printing A as even")
+    if (a > 0) {
+        console.log("Positive")
+    } else {
+        console.log("negative")
+    }
+}
